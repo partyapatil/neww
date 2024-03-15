62a447f3f6b6fe821760250f0c2573105be56ddc
prathamesh <prathameshpatil8579@gmail.com>
2024-03-15 23:52:42 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,355 @@
+import HeroIm from "../images/p3.jpg"
+// import HeroIm from '../../images/p3.jpg'
+import myntra from "../images/myntra-img.png";
+import social from "../images/social-img.png";
+import todo from "../images/to-do-img.png"
+import hero from "../images/p.jpg"
+export const Bio = {
+  name: "Prathamesh Patil",
+  roles: [
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/partyapatil/new.git",
+  resume:
+    "https://drive.google.com/file/d/1MKJv54lBWS6lU81ET9zurzqziNWXqa5Y/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/prathamesh-patil-01431a246",
+  twitter: "https://twitter.com/Pratham28923581?t=5-FG9m02U9jCHv0Eku-r2A&s=08",
+  insta: "https://www.instagram.com/partyapatil9916/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+   
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+   
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://th.bing.com/th/id/OIP.1fZjQpkRMKTBGN_7H5YnFwHaGL?rs=1&pid=ImgDetMain",
+      },
+ 
+ 
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+  
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:"https://th.bing.com/th/id/OIP.n8pa_ux7uUyU9CJrzb1scAHaHa?rs=1&pid=ImgDetMain"
+
+      },
+      
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://th.bing.com/th/id/OIP.Xa0BEkwl0Zx4qnY9lMbD7gHaHa?rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://th.bing.com/th/id/OIP.MTzDO20EGiEBMg0R4-2zAAHaD3?rs=1&pid=ImgDetMain",
+      },
+     
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "DevOps & Fullstack Engineering Intern",
+    company: "Flipr Inovations Pvt. Ltd.",
+    date: "Aug 2023 - July 2023",
+    desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+    skills: [
+      "Docker",
+      "Terraform",
+      "AWS",
+      "EC2",
+      "Portainer",
+      "Nginx",
+      "JavaScript",
+      "TypeScript",
+      "Node Js",
+      " Next Js",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+    role: "Frontend Engineer Intern",
+    company: "Neurobit",
+    date: "June 2023 - Present",
+    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+    role: "Fullstack Externship",
+    company: "Flipr",
+    date: "June 2023 - July 2023",
+    desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Docker",
+      "AWS",
+      "MongoDB",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Open Source Contributor ",
+    company: "GirlScript Summer of Code",
+    date: "May 2023 - Present",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+    doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+  },
+  {
+    id: 3,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70",
+    role: "Android Developer",
+    company: "DSC KIIT",
+    date: "Nov2021 - Present",
+    desc: "As an Android developer at the Google Developers Student Club (GDCS), I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
+    skills: [
+      "Leadership",
+      "Mobile Application Development",
+      "Kotlin",
+      "XML",
+      "Figma",
+    ],
+  },
+  {
+    id: 4,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Rudraksha.jpeg?alt=media&token=8f83f41e-d0a1-486f-9c7f-dd4cd1d09e8d",
+    role: "Android Developer Intern",
+    company: "Rudraksha Welffare Foundation",
+    date: "June 2021 - Oct 2021",
+    desc: "As an Android Developer Intern at Rudraksha Welfare Foundation from June 2021 to October 2021, I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
+    skills: [
+      "Android",
+      "Java",
+      "Kotlin",
+      "XML",
+      "Node Js",
+      "Cloud Firestore",
+      "Firebase",
+      "Figma",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://th.bing.com/th/id/OIP.c3Ch9WoLeNkETR5d7_9VHQAAAA?rs=1&pid=ImgDetMain",
+    school: "Sanjay Ghodawat University, Kolhapur",
+    date: "sep 2022 - june 2024",
+    grade: "7.82 CGPA",
+    desc: "I am currently pursuing a Master's degree in Computer Science and Engineering at Sanjay Ghodawat University",
+    degree: "Master of computer Aplication - MCA, ",
+  },
+  {
+    id: 1,
+    img: "https://th.bing.com/th/id/R.70c3fd96e1987f9b37dccb664b79f7c4?rik=wYhPpgI27FV5qw&riu=http%3a%2f%2fdevchandcollege.org%2fwp-content%2fuploads%2f2018%2f11%2fmonogram.jpg&ehk=xNxQtIFxwYG%2bCcb9WGT8s3sKxQIUj0joTnXMOegGBvs%3d&risl=&pid=ImgRaw&r=0",
+    school: "Devchand collage,Nipani",
+    date: "June 2019 - July 2022",
+    grade: "78.2%",
+    desc: "I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
+    degree: "Bachalor of science",
+  },
+  {
+    id: 2,
+    img: "https://th.bing.com/th/id/R.70c3fd96e1987f9b37dccb664b79f7c4?rik=wYhPpgI27FV5qw&riu=http%3a%2f%2fdevchandcollege.org%2fwp-content%2fuploads%2f2018%2f11%2fmonogram.jpg&ehk=xNxQtIFxwYG%2bCcb9WGT8s3sKxQIUj0joTnXMOegGBvs%3d&risl=&pid=ImgRaw&r=0",
+    school: "Devchand collage, Nipani",
+    date: "Apr 2017 - Apr 2019",
+    grade: "52.3%",
+    degree: "I completed my class 12 high school education at Devchand collage, where I studied Science with Computer Science.(PCM IT)",
+  },
+  {
+    id: 3,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+    school: "Shri G M Sankpal Highschool, Shipewadi",
+    date: "Apr 2017",
+    grade: "71.3%",
+    degree: "(X)",
+  },
+];
+
+export const projects = [
+
+  {
+    id: 0,
+    title: "Social-Media",
+    date: "Dec 2023 - Feb 2024",
+    description:" Solely developed a social media platform using React for the frontend, Express for backend APIs, and MongoDB for data storage. Implemented Redux for state management, ensuring a cohesive user experience..",
+    image:
+      social,
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    category: "web app",
+    github: "https://github.com/partyapatil/social_media_frontend.git",
+    webapp: "https://github.com/partyapatil/social_media_frontend.git",
+    member: [
+      // {
+      //   name: "Prathamesh patil",
+      //   img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+      //   linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+      //   github: "https://github.com/rishavchanda/",
+      // },s
+      {
+        name: "Prathamesh patil",
+        img:hero,
+        linkedin: "https://www.linkedin.com/in/prathamesh-patil-01431a246",
+        github: "https://github.com/partyapatil/new.git",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Myntra",
+    date: "Nov 2023",
+    description:
+      "Independently crafted a Myntra-inspired e-commerce app using React and Redux Toolkit for streamlined state management. Integrated Firebase storage for efficient multimedia handling.",
+    image:
+     myntra,
+    tags: [
+      "React Js",
+      // "MongoDb",
+      // "Node Js",
+      // "Express Js",
+      "Bootstrap"
+,      "Redux",
+ 
+    ],
+    category: "web app",
+    github: "https://github.com/partyapatil/myntra-clone.git",
+    webapp: "https://myntra-clone-e55d.onrender.com/",
+  },
+
+  {
+    id: 5,
+    title: "Todo Web App",
+    date: "Oct 2023",
+    description:
+"Created a minimalist To-Do application using React, providing users with a simple and intuitive interface for task management."   ,
+ image:
+      todo,
+    tags: ["React Js", "Local Storage", "Bootstarp"],
+    category: "web app",
+    github: "https://github.com/partyapatil/to-do-app.git",
+    webapp: "https://github.com/partyapatil/to-do-app.git",
+  },
+  // {
+  //   id: 6,
+  //   title: "Breaking Bad",
+  //   date: "Jun 2021",
+  //   description:
+  //     "A simple react app that shows the characters of the famous TV series Breaking Bad. It uses the Breaking Bad API to fetch the data. It also has a search bar to search for a particular character.",
+  //   image:
+  //     "https://camo.githubusercontent.com/cd07010cbeb90cb1b43a5d6162784326aef02210ef7d41a0f9ae043b3e392378/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f7425323028313534292e706e673f616c743d6d6564696126746f6b656e3d65613439383630632d303435362d343333342d616435372d336239346663303333363263",
+  //   tags: ["React Js", "API", "Axios", "Node JS"],
+  //   category: "web app",
+  //   github: "https://github.com/rishavchanda/Breaking-Bad",
+  //   webapp: "https://breaking-bad-webapp.netlify.app",
+  // },
+  
+ 
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
